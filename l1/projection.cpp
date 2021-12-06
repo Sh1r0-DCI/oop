2427cd3b8c5af9fd2efec0fa94f1daa9a03b1b6e
NeSh1r0 <khamit-7@yandex.ru>
2021-12-06 20:37:20 +0300

    rotation funcs added(not used)

--- l1/projection.cpp
+++ l1/projection.cpp
@@ -10,6 +10,70 @@ static double deg_to_rad(double deg)
     return deg * pi / 180;
 }
 
+static vertices_t matrix_mult(vertices_t point, const double rotate_matrix[3][3])
+{
+    vertices_t temp_vert;
+    temp_vert.x = rotate_matrix[0][0] * point.x +
+            rotate_matrix[0][1] * point.y +
+            rotate_matrix[0][2] * point.z;
+    temp_vert.y = rotate_matrix[1][0] * point.x +
+            rotate_matrix[1][1] * point.y +
+            rotate_matrix[1][2] * point.z;
+    temp_vert.z = rotate_matrix[2][0] * point.x +
+            rotate_matrix[2][1] * point.y +
+            rotate_matrix[2][2] * point.z;
+
+    return temp_vert;
+}
+
+vertices_t x_rotate(vertices_t point, double angle)
+{
+    vertices_t temp_vert;
+    angle = deg_to_rad(angle);
+
+    const double alpha_rotate[3][3] = {
+        1,  0,            0,
+        0,  cos(angle),   -sin(angle),
+        0,  sin(angle),   cos(angle)
+    };
+
+    temp_vert = matrix_mult(point, alpha_rotate);
+
+    return temp_vert;
+}
+
+vertices_t y_rotate(vertices_t point, double angle)
+{
+    vertices_t temp_vert;
+    angle = deg_to_rad(angle);
+
+    const double beta_rotate[3][3] = {
+        cos(angle),    0, sin(angle),
+        0,                  1, 0,
+        -sin(angle),   0, cos(angle)
+    };
+
+    temp_vert = matrix_mult(point, beta_rotate);
+
+    return temp_vert;
+}
+
+vertices_t z_rotate(vertices_t point, double angle)
+{
+    vertices_t temp_vert;
+    angle = deg_to_rad(angle);
+
+    const double gamma_rotate[3][3] = {
+        cos(angle), -sin(angle),    0,
+        sin(angle), cos(angle),     0,
+        0,          0,              1
+    };
+
+    temp_vert = matrix_mult(point, gamma_rotate);
+
+    return temp_vert;
+}
+
 vertices_t screen_projection(vertices_t point)
 {
     std::cout << "\n entered w/{" << point.x << ", " << point.y << ", " << point.z <<"}";
